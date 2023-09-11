ec55469e1ab1d8d9abe2a23f1320ff1ea0397ee0
Nwachukwu Chinaza Esther <105193755+NecyNazy@users.noreply.github.com>
2023-09-11 23:09:05 +0100

    update

--- src/main/java/com/ruggedcoder/hngtask1/controller/InfoController.java
+++ src/main/java/com/ruggedcoder/hngtask1/controller/InfoController.java
@@ -1,2 +1,39 @@
-package com.ruggedcoder.hngtask1.controller;public class InfoController {
+package com.ruggedcoder.hngtask1.controller;
+
+import com.ruggedcoder.hngtask1.response.InfoResponse;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+    public InfoController() {
+    }
+
+    @GetMapping({"/info"})
+    public ResponseEntity<InfoResponse> getInfo(@RequestParam String slackName, @RequestParam int utcOffset, @RequestParam String track) {
+        InfoResponse response = new InfoResponse();
+        LocalDateTime now = LocalDateTime.now(ZoneOffset.UTC);
+        String UtcTime = DayOfWeek.from(now).toString();
+        if (utcOffset >= -2 && utcOffset <= 2) {
+            response.setUtcTime(now.toString());
+            response.setSlackName("Nwachukwu Chinaza Esther");
+            response.setCurrentDay("Monday");
+            response.setUtcTime(now.toString());
+            response.setTrack("Backend");
+            response.setGithubFileUrl("https://github.com/NecyNazy/hngtask1/blob/master/src/main/java/com/ruggedcoder/hngtask1/MyFile.java");
+            response.setGithubRepoUrl("https://github.com/NecyNazy/hngtask1");
+            response.setStatusCode(200);
+            return new ResponseEntity(response, HttpStatus.OK);
+        } else {
+            response.setUtcTime("INVALID UTC OFFSET");
+            return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
--- src/main/java/com/ruggedcoder/hngtask1/response/InfoResponse.java
+++ src/main/java/com/ruggedcoder/hngtask1/response/InfoResponse.java
@@ -1,2 +1,71 @@
-package com.ruggedcoder.hngtask1.response;public class InfoResponse {
+package com.ruggedcoder.hngtask1.response;
+
+
+public class InfoResponse {
+    private String slackName;
+    private String currentDay;
+    private String UtcTime;
+    private String track;
+    private String githubFileUrl;
+    private String githubRepoUrl;
+    private int statusCode;
+
+    public InfoResponse() {
+    }
+
+    public String getSlackName() {
+        return this.slackName;
+    }
+
+    public void setSlackName(String slackName) {
+        this.slackName = slackName;
+    }
+
+    public String getCurrentDay() {
+        return this.currentDay;
+    }
+
+    public void setCurrentDay(String currentDay) {
+        this.currentDay = currentDay;
+    }
+
+    public String getUtcTime() {
+        return this.UtcTime;
+    }
+
+    public void setUtcTime(String utcTime) {
+        this.UtcTime = utcTime;
+    }
+
+    public String getTrack() {
+        return this.track;
+    }
+
+    public void setTrack(String track) {
+        this.track = track;
+    }
+
+    public String getGithubFileUrl() {
+        return this.githubFileUrl;
+    }
+
+    public void setGithubFileUrl(String githubFileUrl) {
+        this.githubFileUrl = githubFileUrl;
+    }
+
+    public String getGithubRepoUrl() {
+        return this.githubRepoUrl;
+    }
+
+    public void setGithubRepoUrl(String githubRepoUrl) {
+        this.githubRepoUrl = githubRepoUrl;
+    }
+
+    public int getStatusCode() {
+        return this.statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
 }
